604d1d69734b189d338dd65454cd7f829c5f33aa
rifat syamlan <rifatsyamlan78@gmail.com>
2024-09-29 17:50:14 +0700

    commit kedua

---
+++ PemilihanBilangan11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PemilihanBilangan11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Sebuah Angka: ");
+        int angka = sc.nextInt();
+
+        String hasil;
+        hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" 
+                                        : "Angka " + angka + " termasuk bilangan ganjil";
+
+        System.out.println(hasil);
+    }
+}
